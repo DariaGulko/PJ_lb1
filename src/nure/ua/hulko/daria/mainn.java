1e05b98c1f5dab87e92c3b1f917d156921db943c
Darusya Gulko <darusya.gulko@gmail.com>
2016-10-23 16:05:09 +0300

    commit test2

--- src/nure/ua/hulko/daria/mainn.java
+++ src/nure/ua/hulko/daria/mainn.java
@@ -3,8 +3,9 @@ package nure.ua.hulko.daria;
 /**
  * Created by User on 15.10.2016.
  */
-public class main {
+public class mainn {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test2");
     }
 }
