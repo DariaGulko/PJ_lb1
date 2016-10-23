77a9543aa32e2dfeb22bb707b828f304c4028482
Darusya Gulko <darusya.gulko@gmail.com>
2016-10-23 15:59:06 +0300

    Initial commit

---
+++ src/nure/ua/hulko/daria/main.java
@@ -0,0 +1,10 @@
+package nure.ua.hulko.daria;
+
+/**
+ * Created by User on 15.10.2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
